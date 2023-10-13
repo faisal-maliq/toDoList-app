65cf4560f65d7515ee10ca9249fc6f65af3ec9b7
faisal-maliq <ifaisalmalik99@gmail.com>
2023-10-13 20:22:25 +0530

    first commit

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [item, setItem] = useState([]);
+  function handleInput(e) {
+    const newInput = e.target.value;
+    setInput(newInput);
+  }
+  function addItem() {
+    setItem((previtems) => {
+      return [...previtems, input];
+    });
+    setInput("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleInput} type="text" value={input} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {item.map((toDoItem) => (
+            <li>{toDoItem}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
